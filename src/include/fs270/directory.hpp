dc18cff687fa7ee90c4dcf4746fa78c766db8795
Fatih BAKIR <bakir@ucsb.edu>
2017-11-18 15:41:18 -0800

    optional fuse build

---
+++ src/include/fs270/directory.hpp
@@ -0,0 +1,28 @@
+//
+// Created by fatih on 11/18/17.
+//
+
+#pragma once
+
+namespace fs
+{
+class directory
+{
+public:
+
+
+
+private:
+};
+
+struct dir_entry
+{
+
+};
+
+class dir_iterator
+{
+
+};
+}
+
