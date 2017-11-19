4f713cbfd52673b3fb6642abed45b9873ec56365
Austin <mcever@umail.ucsb.edu>
2017-11-18 20:14:15 -0800

    second ilist test passing

--- tests/ilist_tests.cpp
+++ tests/ilist_tests.cpp
@@ -39,3 +39,26 @@ TEST_CASE("ilists test", "[fs][inode]")
   REQUIRE(fsi.get_number_inodes() == 0);
 
 }
+
+TEST_CASE("inode_return test", "[fs][inode]")
+{
+  // need to test find_inode, create_inode, get_inode, remove_inode, inode_return???
+  auto blk_dev = fs::tests::get_block_dev();
+  auto fsi = fs::make_fs(std::move(blk_dev), {});
+  std::vector<int32_t> inns; // inode numbers
+  int n_ins = fsi.get_number_inodes();
+
+  {
+    inns.push_back(fsi.create_inode());
+    fs::inode_ptr inp = fsi.get_inode(inns[0]);
+    inp->write(0, "hello world", 11);
+  }
+
+  {
+    fs::inode_ptr inp = fsi.get_inode(inns[0]);
+    std::array<char, 11> buf;
+    inp->read(0, buf.data(), 11);
+    REQUIRE(std::memcmp(buf.data(), "hello world", 11) == 0);
+  }
+
+}
